7546a67f005278c00689ee96a21a1a523cd8f397
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2024-01-28 11:34:09 -0300

    Update inserir_os_veiculo.php

--- php/inserir_os_veiculo.php
+++ php/inserir_os_veiculo.php
@@ -19,10 +19,28 @@ $maintenanceStartDate = $_POST['maintenanceStartDate'];
 $maintenanceStartTime = $_POST['maintenanceStartTime'];
 $maintenanceEndDate = $_POST['maintenanceEndDate'];
 $maintenanceFinishTime = $_POST['maintenanceFinishTime'];
+$maintenanceType = isset($_POST['radio-maintenance']) ? $_POST['radio-maintenance'] : '';
+$cabineCheckbox = isset($_POST['cabineCheckbox']) ? 1 : 0;
+$combustivelCheckbox = isset($_POST['combustivelCheckbox']) ? 1 : 0;
+$direcaoCheckbox = isset($_POST['direcaoCheckbox']) ? 1 : 0;
+// Adicione os demais checkboxes conforme necessário...
+
+// Recuperar os dados adicionais
+$componentesAfetados = $_POST['componentesAfetados'];
+$sintomasDetectados = implode(", ", $_POST['sintomasDetectados']);
+$descricaoDefeitoFalha = $_POST['descricaoDefeitoFalha'];
+$causasDefeitoFalha = $_POST['causasDefeitoFalha'];
 
 // Inserir os dados no banco de dados
-$sql = "INSERT INTO tabela_veiculo (odometer, start_date, start_time, end_date, end_time)
-        VALUES ('$odometerValue', '$maintenanceStartDate', '$maintenanceStartTime', '$maintenanceEndDate', '$maintenanceFinishTime')";
+$sql = "INSERT INTO tabela_veiculo (
+    odometer, start_date, start_time, end_date, end_time, maintenance_type,
+    cabine_checkbox, combustivel_checkbox, direcao_checkbox,
+    componentes_afetados, sintomas_detectados, descricao_defeito_falha, causas_defeito_falha
+) VALUES (
+    '$odometerValue', '$maintenanceStartDate', '$maintenanceStartTime', '$maintenanceEndDate', '$maintenanceFinishTime', '$maintenanceType',
+    '$cabineCheckbox', '$combustivelCheckbox', '$direcaoCheckbox',
+    '$componentesAfetados', '$sintomasDetectados', '$descricaoDefeitoFalha', '$causasDefeitoFalha'
+)";
 
 if ($conn->query($sql) === TRUE) {
     echo "Dados inseridos com sucesso!";
