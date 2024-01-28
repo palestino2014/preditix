6133436ce90d1e1e4106bb054436fceeefa20104
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2024-01-28 11:55:36 -0300

    Update inserir_os_veiculo.php

--- php/inserir_os_veiculo.php
+++ php/inserir_os_veiculo.php
@@ -1,4 +1,5 @@
 <?php
+// Configurações do banco de dados
 include "conexao_bd.php";
 
 // Conectar ao banco de dados
@@ -49,6 +50,39 @@ $causaSobrecargaDeTensao = isset($_POST['causaSobrecargaDeTensaoCheckbox']) ? 1 
 // Outros
 $causaOthersCheckboxValue = isset($_POST['causaOthersCheckboxValue']) ? $_POST['causaOthersCheckboxValue'] : '';
 
+$descricaoCausaDefeitoFalha = $_POST['descricaoCausaDefeitoFalha'];
+$intervencaoMecanica = isset($_POST['intervencaoMecanicaCheckbox']) ? 1 : 0;
+$intervencaoPintura = isset($_POST['intervencaoPinturaCheckbox']) ? 1 : 0;
+$intervencaoUsinagem = isset($_POST['intervencaoUsinagemCheckbox']) ? 1 : 0;
+$intervencaoEletrica = isset($_POST['intervencaoEletricaCheckbox']) ? 1 : 0;
+$intervencaoFunilaria = isset($_POST['intervencaoFunilariaCheckbox']) ? 1 : 0;
+$intervencaoCaldeiraria = isset($_POST['intervencaoCaldeirariaCheckbox']) ? 1 : 0;
+$intervencaoHidraulico = isset($_POST['intervencaoHidraulicoCheckbox']) ? 1 : 0;
+$intervencaoSoldagem = isset($_POST['intervencaoSoldagemCheckbox']) ? 1 : 0;
+$tipoIntervencaoOutrosCheckboxValue = isset($_POST['tipoIntervencaoOthersCheckboxValue']) ? $_POST['tipoIntervencaoOthersCheckboxValue'] : '';
+
+$intervencaoAcoplado = isset($_POST['intervencaoAcopladoCheckbox']) ? 1 : 0;
+$intervencaoDesacoplado = isset($_POST['intervencaoDesacopladoCheckbox']) ? 1 : 0;
+$intervencaoInstalado = isset($_POST['intervencaoInstaladoCheckbox']) ? 1 : 0;
+$intervencaoRearmado = isset($_POST['intervencaoRearmadoCheckbox']) ? 1 : 0;
+$intervencaoSoldado = isset($_POST['intervencaoSoldadoCheckbox']) ? 1 : 0;
+$intervencaoAjustado = isset($_POST['intervencaoAjustadoCheckbox']) ? 1 : 0;
+$intervencaoFabricado = isset($_POST['intervencaoFabricadoCheckbox']) ? 1 : 0;
+$intervencaoLimpeza = isset($_POST['intervencaoLimpezaCheckbox']) ? 1 : 0;
+$intervencaoRecuperacao = isset($_POST['intervencaoRecuperacaoCheckbox']) ? 1 : 0;
+$intervencaoSubstituido = isset($_POST['intervencaoSubstituidoCheckbox']) ? 1 : 0;
+$intervencaoAlinhado = isset($_POST['intervencaoAlinhadoCheckbox']) ? 1 : 0;
+$intervencaoFixado = isset($_POST['intervencaoFixadoCheckbox']) ? 1 : 0;
+$intervencaoLubrificado = isset($_POST['intervencaoLubrificadoCheckbox']) ? 1 : 0;
+$intervencaoReposto = isset($_POST['intervencaoRepostoCheckbox']) ? 1 : 0;
+$intervencaoApertado = isset($_POST['intervencaoApertadoCheckbox']) ? 1 : 0;
+$intervencaoInspecionado = isset($_POST['intervencaoInspecionadoCheckbox']) ? 1 : 0;
+$intervencaoModificado = isset($_POST['intervencaoModificadoCheckbox']) ? 1 : 0;
+$intervencaoRetirado = isset($_POST['intervencaoRetiradoCheckbox']) ? 1 : 0;
+$intervencaoOutrosCheckboxValue = isset($_POST['intervencaoOthersCheckboxValue']) ? $_POST['intervencaoOthersCheckboxValue'] : '';
+
+$descricaoIntervencoes = $_POST['descricaoIntervencoes'];
+
 // Inserir os dados no banco de dados
 $sql = "INSERT INTO tabela_veiculo (
     odometer, start_date, start_time, end_date, end_time, maintenance_type,
@@ -58,7 +92,13 @@ $sql = "INSERT INTO tabela_veiculo (
     causa_sobrecarga_corrente, causa_desalinhamento, causa_falta_protecao, causa_engripamento,
     causa_folga, causa_sobrecarga_peso, causa_subdimensionamento, causa_desbalanceamento,
     causa_desregulamento, causa_fadiga, causa_fora_especificacao, causa_nivel_baixo,
-    causa_rompido, causa_sobrecarga_tensao, causa_outros
+    causa_rompido, causa_sobrecarga_tensao, causa_outros , descricao_causa_defeito_falha,
+    intervencao_mecanica, intervencao_pintura, intervencao_usinagem, intervencao_eletrica, intervencao_funilaria,
+    intervencao_caldeiraria, intervencao_hidraulico, intervencao_soldagem, tipo_intervencao_outros,
+    intervencao_acoplado, intervencao_desacoplado, intervencao_instalado, intervencao_rearmado, intervencao_soldado,
+    intervencao_ajustado, intervencao_fabricado, intervencao_limpeza, intervencao_recuperacao, intervencao_substituido,
+    intervencao_alinhado, intervencao_fixado, intervencao_lubrificado, intervencao_reposto, intervencao_apertado,
+    intervencao_inspecionado, intervencao_modificado, intervencao_retirado, intervencao_outros, descricao_intervencoes
 ) VALUES (
     '$odometerValue', '$maintenanceStartDate', '$maintenanceStartTime', '$maintenanceEndDate', '$maintenanceFinishTime', '$maintenanceType',
     '$cabineCheckbox', '$combustivelCheckbox', '$direcaoCheckbox',
@@ -67,7 +107,13 @@ $sql = "INSERT INTO tabela_veiculo (
     '$causaSobrecargaDeCorrente', '$causaDesalinhamento', '$causaFaltaDeProtecao', '$causaEngripamento',
     '$causaFolga', '$causaSobrecargaDePeso', '$causaSubdimensionamento', '$causaDesbalanceamento',
     '$causaDesregulamento', '$causaFadiga', '$causaForaDeEspecificacao', '$causaNivelBaixo',
-    '$causaRompido', '$causaSobrecargaDeTensao', '$causaOthersCheckboxValue'
+    '$causaRompido', '$causaSobrecargaDeTensao', '$causaOthersCheckboxValue' , '$descricaoCausaDefeitoFalha',
+    '$intervencaoMecanica', '$intervencaoPintura', '$intervencaoUsinagem', '$intervencaoEletrica', '$intervencaoFunilaria',
+    '$intervencaoCaldeiraria', '$intervencaoHidraulico', '$intervencaoSoldagem', '$tipoIntervencaoOutrosCheckboxValue',
+    '$intervencaoAcoplado', '$intervencaoDesacoplado', '$intervencaoInstalado', '$intervencaoRearmado', '$intervencaoSoldado',
+    '$intervencaoAjustado', '$intervencaoFabricado', '$intervencaoLimpeza', '$intervencaoRecuperacao', '$intervencaoSubstituido',
+    '$intervencaoAlinhado', '$intervencaoFixado', '$intervencaoLubrificado', '$intervencaoReposto', '$intervencaoApertado',
+    '$intervencaoInspecionado', '$intervencaoModificado', '$intervencaoRetirado', '$intervencaoOutrosCheckboxValue', '$descricaoIntervencoes'
 )";
 
 if ($conn->query($sql) === TRUE) {
