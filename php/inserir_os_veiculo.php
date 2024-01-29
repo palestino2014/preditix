d79669eaa733b276430d72f709c023b5d3447476
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2024-01-29 12:15:47 -0300

    Update inserir_os_veiculo.php

--- php/inserir_os_veiculo.php
+++ php/inserir_os_veiculo.php
@@ -7,56 +7,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Atribuir valores aos parâmetros antes de preparar a instrução
     $odometerValue = isset($_POST['odometerValue']) ? $_POST['odometerValue'] : '';
-    echo "odometerValue: " . $odometerValue . "<br>";
-
     $maintenanceStartDate = isset($_POST['maintenanceStartDate']) ? $_POST['maintenanceStartDate'] : '';
-    echo "maintenanceStartDate: " . $maintenanceStartDate . "<br>";
-
     $maintenanceStartTime = isset($_POST['maintenanceStartTime']) ? $_POST['maintenanceStartTime'] : '';
-    echo "maintenanceStartTime: " . $maintenanceStartTime . "<br>";
-
     $maintenanceEndDate = isset($_POST['maintenanceEndDate']) ? $_POST['maintenanceEndDate'] : '';
-    echo "maintenanceEndDate: " . $maintenanceEndDate . "<br>";
-
     $maintenanceFinishTime = isset($_POST['maintenanceFinishTime']) ? $_POST['maintenanceFinishTime'] : '';
-    echo "maintenanceFinishTime: " . $maintenanceFinishTime . "<br>";
-
     $maintenanceType = isset($_POST['radio-maintenance']) ? $_POST['radio-maintenance'] : '';
-    echo "maintenanceType: " . $maintenanceType . "<br>";
 
-    $sistemasAfetados = [];
-
-    $checkboxes = [
-        'cabineCheckbox', 'combustivelCheckbox', 'direcaoCheckbox', 'medicaoControleCheckbox', 'protecaoImpactosCheckbox',
-        'transmissaoCheckbox', 'estruturalCheckbox', 'acoplamentoCheckbox', 'controleEletronicoCheckbox', 'exaustaoCheckbox',
-        'propulsaoCheckbox', 'protecaoContraIncendioCheckbox', 'ventilacaoCheckbox', 'tanqueCheckbox', 'arrefecimentoCheckbox',
-        'descargaCheckbox', 'freiosCheckbox', 'protecaoAmbientalCheckbox', 'suspensaoCheckbox', 'eletricoCheckbox'
-    ];
-
-    foreach ($checkboxes as $checkbox) {
-        if (isset($_POST[$checkbox])) {
-            $sistemasAfetados[] = $checkbox;
-        }
-    }
-
-    // Converter o vetor em uma string separada por vírgula
-    $sistemasAfetadosString = implode(", ", $sistemasAfetados);
-
-    echo "String sistemas afetados: " . $sistemasAfetadosString . "<br>";
+    // Verificar as checkboxes
+    $cabineCheckbox = isset($_POST['cabineCheckbox']) ? 1 : 0;
+    $direcaoCheckbox = isset($_POST['direcaoCheckbox']) ? 1 : 0;
+    $combustivelCheckbox = isset($_POST['combustivelCheckbox']) ? 1 : 0;
 
     // Preparar e executar a instrução de inserção no banco de dados
     $stmt = $conn->prepare("INSERT INTO os_veiculo (
-        odometer, start_date, start_time, end_date, end_time, maintenance_type, sistema_afetado) VALUES (
-        ?, ?, ?, ?, ?, ?, ?)");
+        odometer, start_date, start_time, end_date, end_time, maintenance_type, cabine, combustivel, direcao) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
     // Verificar se a preparação da instrução foi bem-sucedida
-    if ($stmt === false) {
+    if (!$stmt) {
         die('Erro na preparação da consulta: ' . $conn->error);
     }
 
     // Modificar a string de definição de tipo para corresponder ao número correto de parâmetros
-    $stmt->bind_param("sssssss",
-        $odometerValue, $maintenanceStartDate, $maintenanceStartTime, $maintenanceEndDate, $maintenanceFinishTime, $maintenanceType, $sistemasAfetadosString);
+    $stmt->bind_param("ssssssiii",
+        $odometerValue, $maintenanceStartDate, $maintenanceStartTime, $maintenanceEndDate, $maintenanceFinishTime, $maintenanceType, $cabineCheckbox, $combustivelCheckbox, $direcaoCheckbox);
 
     // Executar a instrução preparada
     if ($stmt->execute()) {
