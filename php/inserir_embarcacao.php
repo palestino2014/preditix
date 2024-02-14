e1b01f0ec83ef534318f1e68a53635d1698d7c88
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2024-02-14 15:44:49 -0300

    Update inserir_embarcacao.php

--- php/inserir_embarcacao.php
+++ php/inserir_embarcacao.php
@@ -5,11 +5,6 @@ include "conexao_bd.php";
 // Verifica se o formulário foi enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-     // Preparar e executar a inserção no banco de dados
-    $stmt = $conn->prepare("INSERT INTO ativo_embarcacao (tipo_embarcacao, tag , num_inscricao, nome, armador, ano_fabricacao, capacidade_volumetrica, foto) VALUES (?, ? , ?, ?, ?, ?, ?, ?)");
-
-    $stmt->bind_param("sssssiss", $tipo_embarcacao,$tag , $num_inscricao, $nome, $armador, $ano_fabricacao, $capacidade_volumetrica, $foto);
-
     // Atribuir valores aos parâmetros
     $tipo_embarcacao = $_POST["tipo_embarcacao"];
     $tag = $_POST["tag"];
@@ -17,18 +12,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST["nome"];
     $armador = $_POST["armador"];
     $ano_fabricacao = $_POST["ano_fabricacao"];
+    $fabricante = $_POST["fabricante"];
     $capacidade_volumetrica = $_POST["capacidade_volumetrica"];
     $foto = $_FILES["foto"]["name"]; // Nome do arquivo de imagem
 
-    // Executar a instrução preparada
-    if ($stmt->execute()) {
-        echo "<p>Dados inseridos com sucesso.</p>";
+    // Preparar e executar a inserção no banco de dados
+    $stmt = $conn->prepare("INSERT INTO ativo_embarcacao (tipo_embarcacao, tag, num_inscricao, nome, armador, fabricante, ano_fabricacao, capacidade_volumetrica, foto) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+    // Verificar se a preparação foi bem-sucedida
+    if ($stmt) {
+        // Vincular parâmetros
+        $stmt->bind_param("ssssssiss", $tipo_embarcacao, $tag, $num_inscricao, $nome, $armador, $fabricante, $ano_fabricacao, $capacidade_volumetrica, $foto);
+
+        // Executar a instrução preparada
+        if ($stmt->execute()) {
+            echo "<p>Dados inseridos com sucesso.</p>";
+        } else {
+            echo "<p>Erro ao inserir dados: " . $stmt->error . "</p>";
+        }
+
+        // Fechar a instrução preparada
+        $stmt->close();
     } else {
-        echo "<p>Erro ao inserir dados: " . $stmt->error . "</p>";
+        echo "<p>Erro na preparação da instrução: " . $conn->error . "</p>";
     }
 
-    // Fechar a instrução preparada e a conexão
-    $stmt->close();
+    // Fechar a conexão
     $conn->close();
 }
 ?>
+
