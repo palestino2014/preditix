5c285312f5982c2a83ede0107beaa3467076cdc6
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2024-03-26 20:10:33 -0300

    Update inserir_embarcacao.php

--- php/inserir_embarcacao.php
+++ php/inserir_embarcacao.php
@@ -5,6 +5,11 @@ include "conexao_bd.php";
 // Verifica se o formulário foi enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+     // Preparar e executar a inserção no banco de dados
+    $stmt = $conn->prepare("INSERT INTO ativo_embarcacao (tipo_embarcacao, tag , num_inscricao, nome, armador, ano_fabricacao, capacidade_volumetrica, foto) VALUES (?, ? , ?, ?, ?, ?, ?, ?)");
+
+    $stmt->bind_param("sssssiss", $tipo_embarcacao,$tag , $num_inscricao, $nome, $armador, $ano_fabricacao, $capacidade_volumetrica, $foto);
+
     // Atribuir valores aos parâmetros
     $tipo_embarcacao = $_POST["tipo_embarcacao"];
     $tag = $_POST["tag"];
@@ -12,33 +17,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST["nome"];
     $armador = $_POST["armador"];
     $ano_fabricacao = $_POST["ano_fabricacao"];
-    $fabricante = $_POST["fabricante"];
     $capacidade_volumetrica = $_POST["capacidade_volumetrica"];
     $foto = $_FILES["foto"]["name"]; // Nome do arquivo de imagem
+    
+    					{ if (isset($_FILES["foto"]) && $_FILES["foto"]["error"] == 0) 
+										
+										{ $foto_nome = $_FILES["foto"]["name"]; 
+													$foto_tmp = $_FILES["foto"]["tmp_name"]; 
+													$foto_destino = "embarcacao/" . $foto_nome; // diretório onde as imagens serão salvas 
+													move_uploaded_file($foto_tmp, $foto_destino); 
+													$sql = "INSERT INTO fotos (caminho) VALUES ('$foto_destino')"; //inserir na tabela fotos
+										if ($conn->query($sql) === TRUE) { echo "Imagem enviada com sucesso."; } 
+						
+						else  { echo "Erro ao enviar imagem: " . $conn->error; } 
+					} else { echo "Erro no envio do arquivo."; 
+				} 
+			}
+    
 
-    // Preparar e executar a inserção no banco de dados
-    $stmt = $conn->prepare("INSERT INTO ativo_embarcacao (tipo_embarcacao, tag, num_inscricao, nome, armador, fabricante, ano_fabricacao, capacidade_volumetrica, foto) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-
-    // Verificar se a preparação foi bem-sucedida
-    if ($stmt) {
-        // Vincular parâmetros
-        $stmt->bind_param("ssssssiss", $tipo_embarcacao, $tag, $num_inscricao, $nome, $armador, $fabricante, $ano_fabricacao, $capacidade_volumetrica, $foto);
-
-        // Executar a instrução preparada
-        if ($stmt->execute()) {
-            echo "<p>Dados inseridos com sucesso.</p>";
-        } else {
-            echo "<p>Erro ao inserir dados: " . $stmt->error . "</p>";
-        }
-
-        // Fechar a instrução preparada
-        $stmt->close();
+    // Executar a instrução preparada
+    if ($stmt->execute()) {
+        echo "<p>Dados inseridos com sucesso.</p>";
     } else {
-        echo "<p>Erro na preparação da instrução: " . $conn->error . "</p>";
+        echo "<p>Erro ao inserir dados: " . $stmt->error . "</p>";
     }
 
-    // Fechar a conexão
+    // Fechar a instrução preparada e a conexão
+    $stmt->close();
     $conn->close();
 }
 ?>
-
