741f6a5d8fea382fb132e6b17654bc7c4af994dc
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2024-03-26 21:40:23 -0300

    Update inserir_veiculos.php

--- php/inserir_veiculos.php
+++ php/inserir_veiculos.php
@@ -21,6 +21,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $CMT = $_POST["CMT"];
     $cor = $_POST["cor"];
     $foto = $_FILES["foto"]["name"];
+    
+    			{ if (isset($_FILES["foto"]) && $_FILES["foto"]["error"] == 0) 
+										
+										{ $foto_nome = $_FILES["foto"]["name"]; 
+													$foto_tmp = $_FILES["foto"]["tmp_name"]; 
+													$foto_destino = "veiculo/" . $foto_nome; // diretório onde as imagens serão salvas 
+													move_uploaded_file($foto_tmp, $foto_destino); 
+													$sql = "INSERT INTO fotos (caminho) VALUES ('$foto_destino')"; //inserir na tabela fotos
+										if ($conn->query($sql) === TRUE) { echo "Imagem enviada com sucesso."; } 
+						
+						else  { echo "Erro ao enviar imagem: " . $conn->error; } 
+					} else { echo "Erro no envio do arquivo."; 
+				} 
+			}   
 
     // Preparar e executar a inserção no banco de dados
     $stmt = $conn->prepare("INSERT INTO ativo_veiculo (tipo_veiculo, tag, placa, fabricante, modelo, ano_fabricacao, chassis, renavam, proprietario, tara, lotacao, PTB, PBTC, CMT , cor, foto) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? ,?, ?, ?, ?, ?)");
