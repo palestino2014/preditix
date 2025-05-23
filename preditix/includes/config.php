8d0236589d26115bbc405e8aa9014f931a49b203
André <proandremelo@gmail.com>
2025-05-22 21:05:10 -0300

    edit: mudança de ambiente

--- preditix/includes/config.php
+++ preditix/includes/config.php
@@ -1,9 +1,18 @@
 <?php
 // Configurações do banco de dados
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'preditix_v1');
+$ambienteIsRemoto = false;
+
+if($ambienteIsRemoto){
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'autode51_adm');
+    define('DB_PASS', 'bUd@36581259');
+    define('DB_NAME', 'autode51_preditix_v5');
+} else {
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'preditix_v1');
+}
 
 // Configurações do sistema
 // define('BASE_URL', 'http://localhost/preditix_v1');
