0805e782def10a17c83de60b881ec87ce22ec1e9
André <proandremelo@gmail.com>
2025-07-09 15:01:34 -0300

    logs

--- nfeapi/config/container.php
+++ nfeapi/config/container.php
@@ -1,6 +1,5 @@
 <?php
 
-use DI\ContainerBuilder;
 use GuzzleHttp\Client;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
@@ -10,66 +9,70 @@ use NfeApi\Utils\NfeUtils;
 use NfeApi\Validators\NfeValidator;
 use NfeApi\Middleware\ErrorMiddleware;
 
-return function (ContainerBuilder $containerBuilder) {
-    $containerBuilder->addDefinitions([
-        // Configuração do Logger
-        Logger::class => \DI\factory(function () {
-            $logger = new Logger('nfeapi');
-            
-            // Log de erros
-            $logger->pushHandler(new StreamHandler(
-                __DIR__ . '/../logs/error.log',
-                Logger::ERROR
-            ));
-            
-            // Log de acesso
-            $logger->pushHandler(new StreamHandler(
-                __DIR__ . '/../logs/access.log',
-                Logger::INFO
-            ));
-            
-            // Log da SEFAZ
-            $logger->pushHandler(new StreamHandler(
-                __DIR__ . '/../logs/sefaz.log',
-                Logger::DEBUG
-            ));
-            
-            return $logger;
-        }),
+return [
+    // Configuração do Logger
+    Logger::class => \DI\factory(function () {
+        $logger = new Logger('nfeapi');
+        
+        // Criar pasta de logs se não existir
+        $logDir = __DIR__ . '/../logs';
+        if (!is_dir($logDir)) {
+            mkdir($logDir, 0755, true);
+        }
+        
+        // Log de erros
+        $logger->pushHandler(new StreamHandler(
+            $logDir . '/error.log',
+            Logger::ERROR
+        ));
+        
+        // Log de acesso
+        $logger->pushHandler(new StreamHandler(
+            $logDir . '/access.log',
+            Logger::INFO
+        ));
+        
+        // Log da SEFAZ
+        $logger->pushHandler(new StreamHandler(
+            $logDir . '/sefaz.log',
+            Logger::DEBUG
+        ));
+        
+        return $logger;
+    }),
 
-        // Configuração do HTTP Client
-        Client::class => \DI\factory(function () {
-            return new Client([
-                'timeout' => $_ENV['HTTP_TIMEOUT'] ?? 60,
-                'verify' => true, // SSL obrigatório
-                'headers' => [
-                    'User-Agent' => 'NFE-API/1.0',
-                    'Accept' => 'application/json',
-                    'Content-Type' => 'application/json'
-                ]
-            ]);
-        }),
+    // Configuração do HTTP Client
+    Client::class => \DI\factory(function () {
+        return new Client([
+            'timeout' => $_ENV['HTTP_TIMEOUT'] ?? 60,
+            'verify' => true, // SSL obrigatório
+            'headers' => [
+                'User-Agent' => 'NFE-API/1.0',
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json'
+            ]
+        ]);
+    }),
 
-        // Configuração do SefazService
-        SefazService::class => \DI\autowire()
-            ->constructorParameter('httpClient', \DI\get(Client::class))
-            ->constructorParameter('logger', \DI\get(Logger::class)),
+    // Configuração do SefazService
+    SefazService::class => \DI\autowire()
+        ->constructorParameter('httpClient', \DI\get(Client::class))
+        ->constructorParameter('logger', \DI\get(Logger::class)),
 
-        // Configuração do NfeUtils
-        NfeUtils::class => \DI\autowire(),
+    // Configuração do NfeUtils
+    NfeUtils::class => \DI\autowire(),
 
-        // Configuração do NfeValidator
-        NfeValidator::class => \DI\autowire()
-            ->constructorParameter('nfeUtils', \DI\get(NfeUtils::class)),
+    // Configuração do NfeValidator
+    NfeValidator::class => \DI\autowire()
+        ->constructorParameter('nfeUtils', \DI\get(NfeUtils::class)),
 
-        // Configuração do NfeService
-        NfeService::class => \DI\autowire()
-            ->constructorParameter('sefazService', \DI\get(SefazService::class))
-            ->constructorParameter('logger', \DI\get(Logger::class))
-            ->constructorParameter('nfeUtils', \DI\get(NfeUtils::class)),
+    // Configuração do NfeService
+    NfeService::class => \DI\autowire()
+        ->constructorParameter('sefazService', \DI\get(SefazService::class))
+        ->constructorParameter('logger', \DI\get(Logger::class))
+        ->constructorParameter('nfeUtils', \DI\get(NfeUtils::class)),
 
-        // Configuração do ErrorMiddleware
-        ErrorMiddleware::class => \DI\autowire()
-            ->constructorParameter('logger', \DI\get(Logger::class)),
-    ]);
-}; 
+    // Configuração do ErrorMiddleware
+    ErrorMiddleware::class => \DI\autowire()
+        ->constructorParameter('logger', \DI\get(Logger::class)),
+]; 
